3dbc0b0aca6c7071de3dfa3494dbd459fcd8d385
Nacho <juanignaciopetrone@gmail.com>
2022-05-14 22:16:37 +0200

    ordenando un poco

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import { getAssetUrl } from '../../utils/config';
 import './style.scss';
 // import { getAuth, signOut } from 'firebase/auth';
 
 // const auth = getAuth();
 const NavBar = () => (
   <div className="navbar">
+    <div className="navbar__option">
+      <img src={getAssetUrl('./basket.svg')} alt="basket" />
+    </div>
+    <div className="navbar__option">
+      <img src={getAssetUrl('./profile.svg')} alt="profile" />
+    </div>
+
     {/* {!auth.currentUser && <button onClick={() => signOut(auth)}> Close sesion </button>} */}
     <Link to={'/login'}>
       <div className="navbar__link">Login</div>

7afb994272a5d8a25c7bbf42635b0a2bf05a92e8
John Doe <maxiarieloliva@gmail.com>
2022-09-21 10:16:50 -0300

    orderState

--- src/components/checkout/payment/index.tsx
+++ src/components/checkout/payment/index.tsx
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import CartContext from '../../../context/CartContext';
 import { getAssetUrl } from '../../../utils/config';
 import { Formik, Field, Form } from 'formik';
-import { updatePayment } from '../../../utils/resolvers';
+import { getCurrentOrder, updatePayment } from '../../../utils/resolvers';
 import './style.scss';
 
 const Payment = () => {
-  const { user } = useContext(CartContext);
+  const { user, getOrder } = useContext(CartContext);
 
   if (!user) {
     return <></>;
@@ -18,38 +18,41 @@ const Payment = () => {
 
   const submitHandler = (values: typeof initialValues) => {
     updatePayment(user.id, values.picked);
+    getOrder(user.id);
   };
 
   return (
     <Formik classname="payment" initialValues={initialValues} onSubmit={submitHandler}>
       {({ values }) => (
         <Form className="payment__f1">
-          <h1 className='payment__h1'>Choose Payment Method</h1>
+          <h1 className="payment__h1">Choose Payment Method</h1>
           <div>
-            <label className='payment__picked'>
-              <Field className='payment__field' type="radio" name="picked" value="AmazonPay" />
-              <img  className='payment__img' src={getAssetUrl('./payment/amazonPay.svg')} />
+            <label className="payment__picked">
+              <Field className="payment__field" type="radio" name="picked" value="AmazonPay" />
+              <img className="payment__img" src={getAssetUrl('./payment/amazonPay.svg')} />
             </label>
-            <label className='payment__picked'>
-              <Field className='payment__field' type="radio" name="picked" value="maestro" />
-              <img className='payment__img' src={getAssetUrl('./payment/maestro.svg')} />
+            <label className="payment__picked">
+              <Field className="payment__field" type="radio" name="picked" value="maestro" />
+              <img className="payment__img" src={getAssetUrl('./payment/maestro.svg')} />
             </label>
-            <label className='payment__picked'>
-              <Field className='payment__field' type="radio" name="picked" value="mastercard" />
-              <img className='payment__img' src={getAssetUrl('./payment/mastercard.svg')} />
+            <label className="payment__picked">
+              <Field className="payment__field" type="radio" name="picked" value="mastercard" />
+              <img className="payment__img" src={getAssetUrl('./payment/mastercard.svg')} />
             </label>
-            <label className='payment__picked'>
-              <Field className='payment__field' type="radio" name="picked" value="payPal" />
-              <img className='payment__img' src={getAssetUrl('./payment/payPal.svg')} />
+            <label className="payment__picked">
+              <Field className="payment__field" type="radio" name="picked" value="payPal" />
+              <img className="payment__img" src={getAssetUrl('./payment/payPal.svg')} />
             </label>
-            <label className='payment__picked'>
-              <Field className='payment__field' type="radio" name="picked" value="visa" />
-              <img className='payment__img' src={getAssetUrl('./payment/visa.svg')} />
+            <label className="payment__picked">
+              <Field className="payment__field" type="radio" name="picked" value="visa" />
+              <img className="payment__img" src={getAssetUrl('./payment/visa.svg')} />
             </label>
           </div>
-          <div className='payment__textbutton'>Picked: {values.picked}</div>
+          <div className="payment__textbutton">Picked: {values.picked}</div>
 
-          <button className='payment__button' type="submit">Confirm Payment</button>
+          <button className="payment__button" type="submit">
+            Confirm Payment
+          </button>
         </Form>
       )}
     </Formik>

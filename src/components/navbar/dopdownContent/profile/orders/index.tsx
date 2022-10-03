dce651f0237ca1fb1bbe5dcefd860d7c3d343183
Nico-app <nicocamara92@gmail.com>
2022-10-03 13:17:10 -0300

    todo visible

--- src/components/navbar/dopdownContent/profile/orders/index.tsx
+++ src/components/navbar/dopdownContent/profile/orders/index.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../../../context/CartContext";
 import { SimpleOrder } from "../../../../../utils/Type";
+import { ItemList } from "../../../../itemList";
+import './style.scss';
 
 const options = {
   weekday: 'long',
@@ -11,9 +13,11 @@ const options = {
   minute: 'numeric',
 };
 
+
+
 const PreviousOrders = () => {
   const [completedOrders, setCompletedOrders] = useState<SimpleOrder[]>([]);
-  const { user, getCompletedOrders } = useContext(CartContext);
+  const { user, order, getCompletedOrders } = useContext(CartContext);
 
   useEffect(() => {
     if (!user) {
@@ -28,15 +32,18 @@ const PreviousOrders = () => {
 
   return (
 
-    <div className="orderSucerfull">
-      <div className="orderSucerful__filter">Your Orders 1</div>
-      <div>
+    <div className="previousOrders">
+      <div className="previousOrders__title">Your Orders</div>
+      <div className="previousOrders__container">
         {completedOrders.map((order, i) => (
-          <div className="orderSucerful__id" key={i}>
-            {/* <div>{order.total}</div> */}
-
-            {/* <img className="orderSucerful__image" src={order.products.image} alt={order.title}></img> */}
-            <div className="orderSucerful__address">{order.completedAt?.toDate().toLocaleString('es-AR', options as Intl.DateTimeFormatOptions)}</div>
+          <div className="previousOrders__id" key={i}>
+            <div className="previousOrders__address">{order.completedAt?.toDate().toLocaleString('es-AR', options as Intl.DateTimeFormatOptions)}</div>
+            <div>
+              {order.products.map((item, i) => (
+                <ItemList key={i} item={item} />
+              ))}
+            </div>
+            {/* {<h2 className="previousOrders__total">Total: ${order.products.reduce((previous, item) => previous + item.price * item.amount, 0).toFixed(2)}</h2>} */}
           </div>
         ))}
       </div>

e4d2d94db2add3bfd8301fd53c6e8c026bffdcb4
Maxi Oliva <maxiarieloliva@gmail.com>
2022-10-20 11:05:56 -0300

    terminal-debugging

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import morgan from 'morgan';
-import API from './api';
+import API from '../client/utils/api';
 import cors from 'cors';
 
 const app = express();

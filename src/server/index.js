03652d77c93c920c73bbec2aa86307278fa9d086
nacho <juanignaciopetrone@gmail.com>
2022-10-23 15:01:00 +0200

    adremixxxbhjbn22

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,10 @@ import app from './app';
 (async () => {
   try {
     app.listen({ port: config.server.port });
+    // eslint-disable-next-line no-undef
     console.log(`Server ready at http://localhost:${config.server.port} 🚀🚀🚀`);
   } catch (err) {
+    // eslint-disable-next-line no-undef
     console.error(err);
   }
 })();
